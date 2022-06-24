b2013b5e571ba903d6bda0b87c388e9b1226ac04
santiago <santiago.borquez.1705@gmail.com>
2022-06-24 20:58:22 -0300

    sd

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);  // Para que todos los posts vayan a esa ruta. Es un middleware
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://santidev4:fermin1904@cluster0.dnhsy.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+
+// mongoose.set('useFindAndModify', false); // ESTA DEPRECADO
