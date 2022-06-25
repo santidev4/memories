797977b0f1a9ba3bcf1d6a9925b55524422f5dd3
santiago <santiago.borquez.1705@gmail.com>
2022-06-25 17:38:03 -0300

    redux

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Post from "./Post/Post"
 
+import useStyles from "./styles"
+
 export default function Posts(){
+    const classes = useStyles();
     return(
         <>
             <h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
+import { Provider } from "react-redux"; // keep tracks of the store, wich is the global state, and that allows us to access
+                                        // that store from anywhere inside the app
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
 import App from "./App.js"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
