b7f6f822a525dc69b29067dd1975b6523cf91766
santiago <santiago.borquez.1705@gmail.com>
2022-06-24 23:58:34 -0300

    using material-ui

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post"
+
+export default function Posts(){
+    return(
+        <>
+            <h1>
+                Posts
+            </h1>
+            <Post />
+            <Post />
+        </>
+    )
+}
